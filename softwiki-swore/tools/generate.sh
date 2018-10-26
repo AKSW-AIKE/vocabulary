eb68c793e7986e6b76115818492fdac218387030
triechert <thomas.riechert@htwk-leipzig.de>
2018-10-26 23:17:22 +0200

    inital files for swore ontology

---
+++ softwiki-swore/tools/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ONTONAME="req"
+
+ONTOVERSION="1"
+
+echo "Generating Documentation for $ONTONAME/$ONTOVERSION"
+
+mkdir ../html
+rm -R ../html/$ONTONAME/$ONTOVERSION
+
+#generate documentation for Version 1.9
+java -jar widoco-1.4.6-jar-with-dependencies.jar -ontFile ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder ../html/$ONTONAME/$ONTOVERSION/
+
+#warte 5 Sekunden
+echo "Documentation generated. Wait now 5s to copy additional files."  
+sleep 5
+
+#set all files rw
+chmod a+rw -R *
+
+#start post processing
+
+
