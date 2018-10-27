587a48528bc6e4eb2f71efb052983a6461788899
triechert <thomas.riechert@htwk-leipzig.de>
2018-10-28 01:24:41 +0200

    version2

--- softwiki-swore/tools/generate.sh
+++ softwiki-swore/tools/generate.sh
@@ -2,26 +2,23 @@
 
 ONTONAME="req"
 
-ONTOVERSION="1"
+ONTOVERSION="2"
 
 echo "Generating Documentation for $ONTONAME/$ONTOVERSION"
 
-mkdir ../html
-rm -R ../html/$ONTONAME/$ONTOVERSION
+mkdir ../html/$ONTONAME/$ONTOVERSION
+rm -R ../html/$ONTONAME/$ONTOVERSION/*
 
 #generate documentation 
 java -jar widoco-1.4.6-jar-with-dependencies.jar -ontFile ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder ../html/$ONTONAME/$ONTOVERSION/
 
 #generate ontology to visualizey
-java -jar OWL2VOWL-0.3.5-shaded.jar -file ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -output ../html/$ONTONAME/$ONTOVERSION/webvowl/data/ontology.json
+#java -jar OWL2VOWL-0.3.5-shaded.jar -file ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -output ../html/$ONTONAME/$ONTOVERSION/webvowl/data/ontology.json
 
 
 #warte 5 Sekunden
 echo "Documentation generated. Wait now 5s to copy additional files."  
 sleep 5
-
-#set all files rw
-chmod a+rw -R *
 
 #start post processing
 
