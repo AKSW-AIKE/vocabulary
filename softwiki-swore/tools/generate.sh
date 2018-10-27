2e92cd71a8ac2ece49890f53b8448fd14ec72854
triechert <thomas.riechert@htwk-leipzig.de>
2018-10-28 00:25:20 +0200

    generated

--- softwiki-swore/tools/generate.sh
+++ softwiki-swore/tools/generate.sh
@@ -12,6 +12,10 @@ rm -R ../html/$ONTONAME/$ONTOVERSION
 #generate documentation 
 java -jar widoco-1.4.6-jar-with-dependencies.jar -ontFile ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder ../html/$ONTONAME/$ONTOVERSION/
 
+#generate ontology to visualizey
+java -jar OWL2VOWL-0.3.5-shaded.jar -file ../model/$ONTONAME-$ONTOVERSION/ontology.ttl -output ../html/$ONTONAME/$ONTOVERSION/webvowl/data/ontology.json
+
+
 #warte 5 Sekunden
 echo "Documentation generated. Wait now 5s to copy additional files."  
 sleep 5
